b79355ba18a0d92e722bbfdbe8cb70642fd830ee
mkokaras <menioskokaras1@gmail.com>
2024-06-01 15:27:58 +0300

    fix(*): small fixes

--- apps/web/renderer/+onRenderClient.js
+++ apps/web/renderer/+onRenderClient.js
@@ -14,30 +14,10 @@ async function onRenderClient(pageContext) {
   if (!Page) throw new Error('My onRenderClient() hook expects pageContext.Page to be defined');
 
   const container = document.getElementById('root');
-  if (!container) throw new Error('DOM element #react-root not found');
 
-  if (pageContext.isHydration) {
-    defineCustomElements();
-    // const results = await hydrateDocument(document);
-    // console.log(results);
-    // hydrateRoot(container, <PageShell pageContext={pageContext} />)
-  } else {
-    // root = createRoot(container)
-    // root.render(<PageShell pageContext={pageContext} />)
-  }
+  if (!container) throw new Error('DOM element #root not found');
 
-  // const page = (
-  //   <PageShell pageContext={pageContext}>
-  //     <Page />
-  //   </PageShell>
-  // )
-  // if (pageContext.isHydration) {
-  //   root = ReactDOM.hydrateRoot(container, page)
-  // } else {
-  //   if (!root) {
-  //     root = ReactDOM.createRoot(container)
-  //   }
-  //   root.render(page)
-  // }
+  defineCustomElements();
+
   document.title = getPageTitle(pageContext);
 }
